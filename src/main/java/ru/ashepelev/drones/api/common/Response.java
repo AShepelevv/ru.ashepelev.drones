159e1fb67665cfb1f2f0f8091e8c3285d2bce9a7
AShepelev <shepelev.as@phystech.edu>
2022-11-06 12:00:13 +0500

    docs: add docs and api description

--- src/main/java/ru/ashepelev/drones/api/common/Response.java
+++ src/main/java/ru/ashepelev/drones/api/common/Response.java
@@ -1,21 +1,25 @@
 package ru.ashepelev.drones.api.common;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Schema(description = "API response container either data eight error is not null")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Response<D, E> {
+    @Schema(description = "Response data")
     private D data;
+    @Schema(description = "Response error")
     private E error;
 
-    public static <D> Response<D, ?> success(D data) {
+    public static <D> Response<D, Object> success(D data) {
         return new Response<>(data, null);
     }
 
-    public static <E> Response<?, E> error(E error) {
+    public static <E> Response<Object, E> error(E error) {
         return new Response<>(null, error);
     }
 }
--- src/main/java/ru/ashepelev/drones/dto/drone/DroneRegistrationDto.java
+++ src/main/java/ru/ashepelev/drones/dto/drone/DroneRegistrationDto.java
@@ -1,6 +1,7 @@
 package ru.ashepelev.drones.dto.drone;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,18 +12,22 @@ import ru.ashepelev.drones.entity.drone.constants.DroneModel;
 
 import javax.validation.constraints.NotNull;
 
+@Schema(description = "Drone registration data")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class DroneRegistrationDto {
+    @Schema(description = "Drone serial number")
     @NotNull
     @DroneSerialNumber
     String droneSerialNumber;
 
+    @Schema(description = "Drone model")
     @NotNull
     DroneModel droneModel;
 
+    @Schema(description = "Drone weight limit")
     @NotNull
     @DoubleRange(min = 0.0, message = "Drone weight limit must be positive")
     Double weightLimit;
